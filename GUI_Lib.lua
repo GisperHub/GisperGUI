12419ea0738b937080cbd5cb465bf550e38eec7b
GISPER-ZerX <103566704+GisperHub@users.noreply.github.com>
2022-06-09 21:00:16 +0700

    Update GUI_Lib.lua

--- GUI_Lib.lua
+++ GUI_Lib.lua
@@ -5713,9 +5713,9 @@ function library:CreateWindow(options, ...)
 		}
 		local daaata = {{"AddTextbox", "__Designer.Textbox.ImageAssetID", backgroundsection, {
 			Name = "Image Asset ID",
-			Placeholder = "rbxassetid://5553946656",
+			Placeholder = "rbxassetid://",
 			Flag = "__Designer.Background.ImageAssetID",
-			Value = "rbxassetid://5553946656",
+			Value = "rbxassetid://",
 			Callback = updatecolorsnotween
 		}}, {"AddColorpicker", "__Designer.Colorpicker.ImageColor", backgroundsection, {
 			Name = "Image Color",
